2cb37c69e9cba3db31a4b80657523cbd315b11b1
Jun Wei Yip <s10198567@connect.np.edu.sg>
2022-02-06 01:27:22 +0800

    Shopping Cart Basic and Advance feature 1 with design of shopping cart done

--- DeliciousDonuts/shoppingCart.php
+++ DeliciousDonuts/shoppingCart.php
@@ -10,12 +10,13 @@ if (! isset($_SESSION["ShopperID"])) { // Check if user logged in
 	exit;
 }
 
-echo "<div id='myShopCart' style='margin:auto'>"; // Start a container
+echo "<div id='myShopCart container' style='width:1200px;margin:auto'>"; // Start a container
 if (isset($_SESSION["Cart"])) {
+	echo"<center>"	;
+	echo "<p class='page-title' style='font-size:30px;padding:20px;'>Shopping Cart</p>"; 
 	include_once("mysql_conn.php");
-	// To Do 1 (Practical 4): 
 	// Retrieve from database and display shopping cart in a table
-	$qry = "SELECT *, (Price*Quantity) AS Total FROM ShopCartItem WHERE ShopCartID=?";
+	$qry = "SELECT *, (ShopCartItem.Price*ShopCartItem.Quantity) AS Total,ShopCartItem.Quantity AS CQuantity FROM ShopCartItem INNER JOIN Product ON ShopCartItem.ProductID = Product.ProductID WHERE ShopCartID=?";
 	$stmt = $conn->prepare($qry);
 	$stmt->bind_param("i", $_SESSION["Cart"]);
 	$stmt->execute();
@@ -23,36 +24,37 @@ if (isset($_SESSION["Cart"])) {
 	$stmt->close();
 	
 	if ($result->num_rows > 0) {
-		// To Do 2 (Practical 4): Format and display 
 		// the page header and header row of shopping cart page
-		echo "<p class='page-title' style='text-align:center'>Shopping Cart</p>"; 
+		echo "<div style=float:left >"; // Start a container
 		echo "<div class='table-responsive' >"; // Bootstrap responsive table
-		echo "<table class='table table-hover'>"; // Start of table
-		echo "<thead class='cart-header'>";
+		echo "<table class='table table-hover' style='border:0.5px solid lightgray'>"; // Start of table
+		echo "<thead class='cart-header' style='background-color:#fec0cc'>";
 		echo "<tr>";
-		echo "<th width='250px'>Item</th>";
-		echo "<th width='90px'>Price (S$)</th>";
+		echo "<th width='300px' colspan='2'>Product</th>";
 		echo "<th width='60px'>Quantity</th>";
-		echo "<th width='120px'>Total (S$)</th>";
+		echo "<th width='90px'>Price</th>";
+		echo "<th width='120px'>Total</th>";
 		echo "<th>&nbsp;</th>";
 		echo "</tr>";
 		echo "</thead>";
-		// To Do 5 (Practical 5):
 		// Declare an array to store the shopping cart items in session variable 
 		$_SESSION["Items"]=array();	
-		// To Do 3 (Practical 4): 
 		// Display the shopping cart content
 		$subTotal = 0; // Declare a variable to compute subtotal before tax
+		$totalQuantity = 0; // Declare a variable to compute number of items
 		echo "<tbody>"; // Start of table's body section
 		while ($row = $result->fetch_array()) {
 			echo "<tr>";
+			$img = "./Images/products/$row[ProductImage]";
+			echo "<td style='width:10%'><input type='image' src=$img title='Product Picture' width='100%' height='100%'/>";
+			echo "</td>";
 			echo "<td style='width:50%'>$row[Name]<br />";
 			echo "Product ID: $row[ProductID]</td>";
 			echo "<td>";
 			echo "<form action='cartFunctions.php' method='post'>";
 			echo "<select name='quantity' onChange='this.form.submit()'>";
 			for ($i = 1; $i <= 10; $i++) {
-				if ($i == $row["Quantity"])
+				if ($i == $row["CQuantity"])
 					$selected = "selected";
 				else
 					$selected = "";
@@ -63,51 +65,62 @@ if (isset($_SESSION["Cart"])) {
 			echo "<input type='hidden' name='product_id' value='$row[ProductID]' />";
 			echo "</form>";
 			echo "</td>";
+			echo "<td>$$row[Price]</td>";
 			$formattedTotal = number_format($row["Total"], 2);
-			echo "<td>$formattedTotal</td>";
+			echo "<td>$$formattedTotal</td>";
 			echo "<td>";
 			echo "<form action='cartFunctions.php' method='post'>";
 			echo "<input type='hidden' name='action' value='remove' />";
 			echo "<input type='hidden' name='product_id' value='$row[ProductID]' />";
-			echo "<input type='image' src='images/trash-can.png' title='Remove Item'/>";
+			echo "<input type='image' src='images/close.png' title='Remove Item' width='13px' height='13px'/>";
 			echo "</form>";
 			echo "</td>";
 			echo "</tr>";	
-			// To Do 6 (Practical 5):
 		    // Store the shopping cart items in session variable as an associate array
 			$_SESSION["Items"][] = array("productId"=>$row["ProductID"],
 										 "name"=>$row["Name"],
 										 "price"=>$row["Price"],
-										 "quantity"=>$row["Quantity"]);
+										 "quantity"=>$row["CQuantity"]);
 
 			// Accumulate the running sub-total
 			$subTotal += $row["Total"];
+			$totalQuantity += $row["CQuantity"];
 		}
 		echo "</tbody>"; // End of table's body section
 		echo "</table>"; // End of table
 		echo "</div>"; // End of Bootstrap responsive table
-				
-		// To Do 4 (Practical 4): 
-		// Display the subtotal at the end of the shopping cart
-		echo "<p style='text-align:right; font-size:20px'>
-			  Subtotal = S$". number_format($subTotal, 2);
+		echo "</div>"; // End of Bootstrap responsive table
+
+		echo "<div style='float:right;margin-left:5px;width:30%;border:0.5px solid lightgray'>"; // Start a container for checkout
+		// Display the subtotal & total Quantity at the end of the shopping cart
+	
+		echo "<p style='text-align:left; font-size:25px;padding-top:5px;padding-left:20px'>CART TOTALS";
+		echo "<p style='text-align:left; font-size:15px;padding-left:20px'>
+			  Total Item(s): &nbsp;&nbsp;&nbsp;&nbsp;". $totalQuantity;
+		echo "<p style='text-align:left; font-size:15px;padding-left:20px'>
+			  Subtotal   S$: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;".  number_format($subTotal, 2);
+		
 		$_SESSION["SubTotal"] = round($subTotal, 2);	
-		// To Do 7 (Practical 5):
+		if ($subTotal > 50)
+		{
+			echo "<p style='text-align:left; font-size:15px;color:red;padding-left:20px'>Normal Delivery Charges waived!";
+		}
 		// Add PayPal Checkout button on the shopping cart page
 		echo "<form method='post' action='checkout.php'>";
-		/*echo "<input type='image' style='float:right;'
-					 src='https://www.paypal.com/en_US/i/btn/btn_xpressCheckout.gif'>";*/
-		echo "<button type='submit' style='float: right; background-color: rgb(0, 132, 255); color: white; font-weight: 500; font-size: 18px; height: 50px; padding: 0 30px; border: none; border-radius: 5px; cursor: pointer;'>Checkout</button>";
+		
+		echo "<button type='submit' style='float: left;margin-left:20px;margin-bottom:10px; background-color: rgb(0, 132, 255); font-weight: 500; font-size: 18px; height: 50px; padding: 0 30px; border: none; border-radius: 5px; cursor: pointer;'>Proceed to Checkout</button>";
 		echo "</form></p>";		
+		echo "</div>"; // End of container
 	}
 	else {
 		echo "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
 	}
 	$conn->close(); // Close database connection
+	
 }
 else {
 	echo "<h3 style='text-align:center; color:red;'>Empty shopping cart!</h3>";
-}
-echo "</div>"; // End of container
+}	
+		echo "</div>"; // End of container
 include("footer.php"); // Include the Page Layout footer
 ?>
