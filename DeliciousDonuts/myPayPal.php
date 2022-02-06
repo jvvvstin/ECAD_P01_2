40a2014c97f1b15db41e45d7b17e04061f15378d
jvvvstin <63656476+jvvvstin@users.noreply.github.com>
2022-02-06 13:00:30 +0800

    Update myPayPal.php

--- DeliciousDonuts/myPayPal.php
+++ DeliciousDonuts/myPayPal.php
@@ -7,7 +7,7 @@ $PayPalCurrencyCode = 'SGD'; 	//Paypal Currency Code
 //URL to redirect to after PayPal has complete the online payment
 $PayPalReturnURL 	= 'http://localhost:8081/DeliciousDonuts/checkoutProcess.php'; 
 //URL to redirect to if user clicks cancel                
-$PayPalCancelURL 	= 'http://localhost:8081/DeliciousDonuts/reviewOrder.php'; 
+$PayPalCancelURL 	= 'http://localhost:8081/DeliciousDonuts/checkout.php'; 
                 	
 function PPHttpPost($methodName_, $nvpStr_, $PayPalApiUsername, $PayPalApiPassword, 
                     $PayPalApiSignature, $PayPalMode) {
